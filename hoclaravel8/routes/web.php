657e0074092e6b7a117b9c1cc300b0fc70af98dd
Ahryxx <phvanthanh06@gmail.com>
2025-10-18 13:51:40 +0700

    Add routes for post management: add, update, and delete

--- hoclaravel8/routes/web.php
+++ hoclaravel8/routes/web.php
@@ -26,4 +26,7 @@ Route::prefix('users')->name('users.')->group(function () {
 
 Route::prefix('posts')->name('posts.')->group(function () {
     Route::get('/', [PostController::class, 'index'])->name('index');
+    Route::get('add', [PostController::class, 'add'])->name('add');
+    Route::get('update/{id}', [PostController::class, 'update'])->name('update');
+    Route::get('delete/{id}', [PostController::class, 'delete'])->name('delete');
 });
